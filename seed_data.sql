baaa0100d08bea9e3363f9d00b9caaac8e31a8bf
Wiącek Arkadiusz <35027@uth.lokalne>
2026-02-01 15:51:01 +0100

    Usuwa tworzenie struktury bazy danych z pliku 'seed'

--- seed_data.sql
+++ seed_data.sql
@@ -1,10 +1,4 @@
 BEGIN TRANSACTION;
-CREATE TABLE artysci (
-    IdArtysty INTEGER PRIMARY KEY AUTOINCREMENT,
-    Nazwa TEXT NOT NULL UNIQUE,
-    Imie TEXT,
-    Nazwisko TEXT
-);
 INSERT INTO "artysci" VALUES(1,'Echoes','Marek','Nowak');
 INSERT INTO "artysci" VALUES(2,'The Soundmakers','Anna','Kowalska');
 INSERT INTO "artysci" VALUES(3,'Deep Resonance','Piotr','Wiśniewski');
@@ -32,11 +26,6 @@ INSERT INTO "artysci" VALUES(24,'A',NULL,NULL);
 INSERT INTO "artysci" VALUES(25,'FindMe',NULL,NULL);
 INSERT INTO "artysci" VALUES(26,'After','New',NULL);
 INSERT INTO "artysci" VALUES(27,'Nowy Artysta','New','Artist');
-CREATE TABLE inzynierowie (
-    IdInzyniera INTEGER PRIMARY KEY AUTOINCREMENT,
-    Imie TEXT NOT NULL,
-    Nazwisko TEXT
-);
 INSERT INTO "inzynierowie" VALUES(1,'Michał','Kowal');
 INSERT INTO "inzynierowie" VALUES(2,'Ewa','Błaszczyk');
 INSERT INTO "inzynierowie" VALUES(3,'Rafał','Majewski');
@@ -58,15 +47,6 @@ INSERT INTO "inzynierowie" VALUES(18,'Eustachy','Motyka');
 INSERT INTO "inzynierowie" VALUES(19,'Jan','Kowalski');
 INSERT INTO "inzynierowie" VALUES(20,'Adam','Nowak');
 INSERT INTO "inzynierowie" VALUES(21,'Genowefa','Pigwowska');
-CREATE TABLE sesje (
-    IdSesji INTEGER PRIMARY KEY AUTOINCREMENT,
-    IdArtysty INTEGER NOT NULL,
-    IdInzyniera INTEGER NOT NULL,
-    TerminStart TEXT NOT NULL,
-    TerminStop TEXT,
-    FOREIGN KEY (IdArtysty) REFERENCES artysci(IdArtysty),
-    FOREIGN KEY (IdInzyniera) REFERENCES inzynierowie(IdInzyniera)
-);
 INSERT INTO "sesje" VALUES(1,1,1,'2025-01-10 10:00:00','2025-01-10 14:00:00');
 INSERT INTO "sesje" VALUES(2,2,3,'2025-01-12 09:00:00','2025-01-12 17:00:00');
 INSERT INTO "sesje" VALUES(3,3,5,'2025-01-15 11:00:00','2025-01-15 18:00:00');
@@ -90,12 +70,6 @@ INSERT INTO "sesje" VALUES(20,15,18,'2025-11-30','2025-12-31');
 INSERT INTO "sesje" VALUES(21,1,1,'2025-01-01',NULL);
 INSERT INTO "sesje" VALUES(22,1,1,'2025-01-01',NULL);
 INSERT INTO "sesje" VALUES(23,21,19,'2025-01-01',NULL);
-CREATE TABLE sprzet (
-    IdSprzetu INTEGER PRIMARY KEY AUTOINCREMENT,
-    Producent TEXT,
-    Model TEXT NOT NULL,
-    Kategoria TEXT CHECK(Kategoria IN ('Mikrofony','Procesory','Przewody','Akcesoria'))
-);
 INSERT INTO "sprzet" VALUES(1,'Shure','SM7B','Mikrofony');
 INSERT INTO "sprzet" VALUES(2,'Neumann','U87','Mikrofony');
 INSERT INTO "sprzet" VALUES(3,'Rode','NT1-A','Mikrofony');
@@ -121,13 +95,6 @@ INSERT INTO "sprzet" VALUES(22,'DBX','166XL','Procesory');
 INSERT INTO "sprzet" VALUES(23,'Behringer','Tube Ultragain','Procesory');
 INSERT INTO "sprzet" VALUES(24,'Motu','828MK2','Procesory');
 INSERT INTO "sprzet" VALUES(25,'Boss','GT-8','Procesory');
-CREATE TABLE sprzety_sesje (
-    IdSprzetu INTEGER NOT NULL,
-    IdSesji INTEGER NOT NULL,
-    PRIMARY KEY (IdSprzetu, IdSesji),
-    FOREIGN KEY (IdSprzetu) REFERENCES sprzet(IdSprzetu),
-    FOREIGN KEY (IdSesji) REFERENCES sesje(IdSesji)
-);
 INSERT INTO "sprzety_sesje" VALUES(1,1);
 INSERT INTO "sprzety_sesje" VALUES(2,2);
 INSERT INTO "sprzety_sesje" VALUES(3,3);
@@ -162,14 +129,6 @@ INSERT INTO "sprzety_sesje" VALUES(1,21);
 INSERT INTO "sprzety_sesje" VALUES(2,21);
 INSERT INTO "sprzety_sesje" VALUES(1,22);
 INSERT INTO "sprzety_sesje" VALUES(2,22);
-CREATE TABLE utwory (
-    IdUtworu INTEGER PRIMARY KEY AUTOINCREMENT,
-    IdArtysty INTEGER NOT NULL,
-    IdSesji INTEGER NOT NULL,
-    Tytul TEXT NOT NULL,
-    FOREIGN KEY (IdArtysty) REFERENCES artysci(IdArtysty),
-    FOREIGN KEY (IdSesji) REFERENCES sesje(IdSesji)
-);
 INSERT INTO "utwory" VALUES(1,1,1,'Fading Echo');
 INSERT INTO "utwory" VALUES(2,2,2,'Light and Sound');
 INSERT INTO "utwory" VALUES(3,3,3,'Deep Blue');
@@ -194,76 +153,4 @@ INSERT INTO "utwory" VALUES(21,15,20,'Mandat pod Wycieraczką');
 INSERT INTO "utwory" VALUES(22,15,20,'Neonówka Migocze o 4:17');
 INSERT INTO "utwory" VALUES(23,12,15,'Najs');
 INSERT INTO "utwory" VALUES(24,12,15,'Ballada o Paramonowie');
-CREATE VIEW v_raport_sesji AS
-SELECT
-  s.IdSesji AS IdSesji,
-  a.Nazwa || ' ' || a.Imie || ' ' || a.Nazwisko AS Artysta,
-  s.TerminStart AS "Rospoczęcie Sesji",
-  sp.Model,
-  sp.Kategoria
-FROM sesje s
-JOIN artysci a ON a.IdArtysty = s.IdArtysty
-JOIN sprzety_sesje ss ON ss.IdSesji = s.IdSesji
-JOIN sprzet sp ON sp.IdSprzetu = ss.IdSprzetu
-GROUP BY s.IdSesji;
-CREATE INDEX idx_sesje ON sesje(IdArtysty);
-CREATE INDEX idx_utwory ON sesje(IdArtysty);
-CREATE TRIGGER validate_sesja_time
-BEFORE INSERT ON sesje
-FOR EACH ROW
-WHEN NEW.TerminStop IS NOT NULL AND NEW.TerminStop <= NEW.TerminStart
-BEGIN
-    SELECT RAISE(ABORT, 'TerminStop musi był póniejszy niż TerminStart');
-END;
-CREATE TRIGGER validate_sesja_time_update
-BEFORE UPDATE ON sesje
-FOR EACH ROW
-WHEN NEW.TerminStop IS NOT NULL AND NEW.TerminStop <= NEW.TerminStart
-BEGIN
-    SELECT RAISE(ABORT, 'TerminStop musi był póniejszy niż TerminStart');
-END;
-CREATE TRIGGER check_inzynier_overlap
-BEFORE INSERT ON sesje
-FOR EACH ROW
-BEGIN
-    SELECT RAISE(ABORT, 'Inżynier jest już zajęty w tym terminie')
-    WHERE EXISTS (
-        SELECT 1 FROM sesje
-        WHERE IdInzyniera = NEW.IdInzyniera
-        AND IdSesji != NEW.IdSesji
-        AND (
-            (NEW.TerminStart >= TerminStart AND NEW.TerminStart < TerminStop)
-            OR (NEW.TerminStop > TerminStart AND NEW.TerminStop <= TerminStop)
-            OR (NEW.TerminStart <= TerminStart AND NEW.TerminStop >= TerminStop)
-        )
-    );
-END;
-CREATE TRIGGER validate_utwor_artist
-BEFORE INSERT ON utwory
-FOR EACH ROW
-BEGIN
-    SELECT RAISE(ABORT, 'IdArtysty w utworze musi zgadzać się z IdArtysty w sesji')
-    WHERE NOT EXISTS (
-        SELECT 1 FROM sesje 
-        WHERE IdSesji = NEW.IdSesji 
-        AND IdArtysty = NEW.IdArtysty
-    );
-END;
-CREATE TRIGGER validate_utwor_artist_update
-BEFORE UPDATE ON utwory
-FOR EACH ROW
-BEGIN
-    SELECT RAISE(ABORT, 'IdArtysty w utworze musi zgadzać się z IdArtysty w sesji')
-    WHERE NOT EXISTS (
-        SELECT 1 FROM sesje 
-        WHERE IdSesji = NEW.IdSesji 
-        AND IdArtysty = NEW.IdArtysty
-    );
-END;
-DELETE FROM "sqlite_sequence";
-INSERT INTO "sqlite_sequence" VALUES('artysci',27);
-INSERT INTO "sqlite_sequence" VALUES('inzynierowie',21);
-INSERT INTO "sqlite_sequence" VALUES('sesje',23);
-INSERT INTO "sqlite_sequence" VALUES('sprzet',25);
-INSERT INTO "sqlite_sequence" VALUES('utwory',24);
 COMMIT;
